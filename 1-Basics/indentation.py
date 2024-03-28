d25423eaa7bac88133213587c6cefa62733ac6fc
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-03-28 19:58:18 +0530

    add hello.py and indentation.py

---
+++ 1-Basics/indentation.py
@@ -0,0 +1,22 @@
+# syntax error code --------------------------------------------------------------
+# if 5 > 2: 
+# print("hello world")
+
+# add space ----------------------------------------------------------------------
+if 5 > 2:
+ print("5 is greater than 2!") 
+
+if 8 > 2:
+        print("8 is greater than 2!") 
+
+# add same number of space in same block of code ---------------------------------     
+if 10 > 5: 
+    print("10 is greater than 5")
+#       print("10 is greater than 5") #error line
+
+# multiline comment --------------------------------------------------------------
+"""
+This is a comment
+written in
+more than just one line
+"""
