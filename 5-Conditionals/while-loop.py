21f89017b0c58c305685b5306e1113e74659bd22
nirajkumardangi <niraj8825224435@gmail.com>
2026-06-25 07:10:15 +0530

    Remove commented-out while loop examples and clean up code structure

--- 5-Conditionals/while-loop.py
+++ 5-Conditionals/while-loop.py
@@ -1,5 +1,19 @@
 # While Loop
-number = 1
-while number <= 5:
-  print(number)
-  number = number + 1
+# number = 1
+# while number <= 5:
+#   print(number)
+#   number = number + 1
+  
+  
+# Q1. Write a while loop that prints the numbers from 1 to 10.
+# num = 1
+# while num <= 10:
+#   print(num)
+#   num += 1
+  
+# Q2. Write a while loop that seperates digit from number
+num = 12345
+while num > 0:
+  digit = num % 10
+  print(digit)
+  num = num // 10 
