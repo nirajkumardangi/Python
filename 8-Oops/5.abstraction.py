19b4c79b41a198bf374e0a30e4f3ba6979cb22fc
nirajkumardangi <niraj8825224435@gmail.com>
2026-09-07 13:44:26 +0530

    learning abstraction

--- 8-Oops/5.abstraction.py
+++ 8-Oops/5.abstraction.py
@@ -1,5 +1,39 @@
 # abstraction: hiding implementation details of a class and only showing only essential feature to the user.
 
+from abc import ABC, abstractmethod
+
+class TalkingToy(ABC):
+    def __init__(self, name):
+        self.name = name
+        
+    @abstractmethod
+    def speak(self):
+        pass
+
+class RobotToy(TalkingToy):
+    def speak(self):
+        print(f'{self.name} says beep boop! I am a robot!')
+
+class TeddyBearToy(TalkingToy):
+    def speak(self):
+        print(f"{self.name} says hug me! I'm cuddly!")
+
+class DinosaurToy(TalkingToy):
+    def speak(self):
+        print(f'{self.name} says ROOOOAR!')
+
+# Toys create karna
+rusty = RobotToy('Rusty') # Rusty says beep boop! I am a robot!
+fluffy = TeddyBearToy('Fluffy') # Fluffy says hug me! I'm cuddly!
+rex = DinosaurToy('Rex') # Rex says ROOOOAR!
+
+toys = [rusty, fluffy, rex]
+for toy in toys:
+    toy.speak()
+    
+    
+"""---------------------------------------------"""    
+
 class Car:
   def __init__(self):
     self.acc = False
